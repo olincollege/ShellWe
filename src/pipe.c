c08f15f3daa62799572047c84139f0b6a91d9265
Ian <ianlum314@gmail.com>
2024-04-12 19:32:04 -0400

    two way pipe

--- src/pipe.c
+++ src/pipe.c
@@ -7,24 +7,34 @@
 #include "util.h"
 
 int main(void) {
-  int pipe_fd[2];
-  if (pipe(pipe_fd) == -1) {
+  const int BUFFER_SIZE = 80;
+  char buffer[BUFFER_SIZE];
+  int pipe_to_parent[2];
+  int pipe_to_child[2];
+  if (pipe(pipe_to_parent) == -1 || pipe(pipe_to_child) == -1) {
     error_and_exit("Can't create pipe");
   }
+
   pid_t pid = fork();
   if (pid == -1) {
     error_and_exit("Can't fork process");
   }
+
   if (!pid) {
     // child
-    char msg[] = "hello";
-    write(pipe_fd[1], msg, strlen(msg));
+    char msg[] = "from child to parent";
+    write(pipe_to_parent[1], msg, strlen(msg));
+
+    read(pipe_to_child[0], buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
+  } else {
+    // parent
+    char msg[] = "from parent to child";
+    write(pipe_to_child[1], msg, strlen(msg));
+
+    read(pipe_to_parent[0], buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
   }
-  // parent
-  const int BUFFER_SIZE = 80;
-  char buffer[BUFFER_SIZE];
-  read(pipe_fd[0], buffer, BUFFER_SIZE);
-  printf("%s\n", buffer);
 
   return 0;
 }
