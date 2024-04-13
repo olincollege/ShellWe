4a8056df0ed6929584210bc437c16144126b4157
Ian <ianlum314@gmail.com>
2024-04-12 20:53:08 -0400

    receive from multiple children issue: loops through and waits to receive from each child, cant recieve from only once child receives some extra characters?

--- src/pipe.c
+++ src/pipe.c
@@ -6,33 +6,40 @@
 
 #include "util.h"
 
-int main(void) {
-  const int BUFFER_SIZE = 80;
-  char buffer[BUFFER_SIZE];
-  int pipe_to_parent[2];
-  int pipe_to_child[2];
-  if (pipe(pipe_to_parent) == -1 || pipe(pipe_to_child) == -1) {
-    error_and_exit("Can't create pipe");
-  }
-
+void spawn_child(int child_id, int* pipe_fd) {
   pid_t pid = fork();
-  if (pid == -1) {
+
+  if (pid < 0) {
     error_and_exit("Can't fork process");
   }
+  if (pid == 0) {
+    char msg[30];
+    if (snprintf(msg, sizeof(msg), "I am child %i", child_id) < 0) {
+      error_and_exit("Error making message string");
+    }
+    write(pipe_fd[1], msg, strlen(msg));
+    exit(EXIT_SUCCESS);
+  }
+}
 
-  if (!pid) {
-    // child
-    char msg[] = "from child to parent";
-    write(pipe_to_parent[1], msg, strlen(msg));
+int main(void) {
+  const int NUM_CHILDREN = 3;
+  int pipe_to_parent[NUM_CHILDREN][2];
 
-    read(pipe_to_child[0], buffer, BUFFER_SIZE);
-    printf("%s\n", buffer);
-  } else {
-    // parent
-    char msg[] = "from parent to child";
-    write(pipe_to_child[1], msg, strlen(msg));
+  for (int i = 0; i < NUM_CHILDREN; ++i) {
+    if (pipe(pipe_to_parent[i]) == -1) {
+      error_and_exit("Can't create pipe");
+    }
+  }
 
-    read(pipe_to_parent[0], buffer, BUFFER_SIZE);
+  for (int i = 0; i < NUM_CHILDREN; ++i) {
+    spawn_child(i, pipe_to_parent[i]);
+  }
+
+  for (int i = 0; i < NUM_CHILDREN; ++i) {
+    const int BUFFER_SIZE = 80;
+    char buffer[BUFFER_SIZE];
+    read(pipe_to_parent[i][0], buffer, BUFFER_SIZE);
     printf("%s\n", buffer);
   }
 
