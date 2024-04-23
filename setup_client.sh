daa2d47e847c1b056b3de6bf9de524111025d4c4
Daeyoung Kim <corydykim@gmail.com>
2024-04-23 15:55:48 -0400

    Remove some packages and take server ip as arg

--- setup_client.sh
+++ setup_client.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install -y gcc gdb make cmake clang-format \
-clang-tidy libcriterion-dev jq
-git clone --single-branch --branch aws https://github.com/olincollege/ShellWe.git
+sudo apt install -y gcc make cmake clang-format \
+clang-tidy libcriterion-dev
+git clone --single-branch --branch main https://github.com/olincollege/ShellWe.git
 cd ShellWe || exit
 mkdir build && cd build || exit
 cmake ..
 make
 cd src/client || exit
-ip_addr="3.130.117.42"
+ip_addr="$1"
 ./ChatClient "$ip_addr"
