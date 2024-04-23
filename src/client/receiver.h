7d3772e501974730f69e0d66599d7366cbbd322c
jcuhnoio <junochoi2003@gmail.com>
2024-04-23 15:40:43 -0400

    Docstrings written for header files

--- src/client/receiver.h
+++ src/client/receiver.h
@@ -1,3 +1,14 @@
 #pragma once
 
+/**
+ * Receive messages from a socket and print them to stdout.
+ *
+ * Continuously receieves messages from the provided socket, then prints it out
+ * to stdout. Terminates if there is an error during receiving or the socket is
+ * closed.
+ *
+ * @param socket_desc A void pointer to the socket descriptor.
+ * @returns The Null pointer.
+ */
+
 void* receive_message(void* socket_desc);
--- src/server/client_handler.h
+++ src/server/client_handler.h
@@ -12,8 +12,14 @@ typedef struct {
 } client_handler_args_t;
 
 /**
+ * Handle messages from a client and broadcast them to other clients.
  *
- *
+ * Handles messages received from a client socket, broadcasting
+ * the messages to all other connected client sockets. It also manages the
+ * removal of the client from the list of connected clients when the client
+ * disconnects. Terminates when the client socket is closed or an error occurs 
+ * during receiving.
+ * 
  * @param arg A client_handler_args_t pointer to the function arguments.
  * @returns The null pointer
  */
