fb00b750365ce8c16e94e4109c9c90368f9d1192
Ian <ianlum314@gmail.com>
2024-04-23 22:52:32 -0400

    add functions to header

--- src/server/server.h
+++ src/server/server.h
@@ -13,3 +13,8 @@ typedef struct {
 server_t* init_server(struct sockaddr_in ip_addr, int max_backlog);
 
 void listen_for_connections(server_t* server);
+
+int accept_client(server_t* server);
+
+void log_client(int socket_fd, int* n_clients, int* client_sockets,
+                pthread_mutex_t* clients_mutex);
