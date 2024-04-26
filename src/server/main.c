9452a1ce967e8982609c0fec3ddd5521993d7cdb
Ian <ianlum314@gmail.com>
2024-04-26 15:58:32 -0400

    reorder and add comments

--- src/server/main.c
+++ src/server/main.c
@@ -11,15 +11,16 @@
 #include "server.h"
 
 int main(void) {
-  int client_sockets[MAX_CLIENTS];
-  int* n_clients = malloc(sizeof(int));
-  *n_clients = 0;
-  pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-
   // init server
   struct sockaddr_in server_addr = socket_address(SERVER_IP, PORT);
   server_t* server = init_server(server_addr, MAX_BACKLOG);
   listen_for_connections(server);
+
+  // init variables for tracking clients
+  int client_sockets[MAX_CLIENTS];
+  int* n_clients = malloc(sizeof(int));
+  *n_clients = 0;
+  pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
   while (1) {
     int new_socket = accept_client(server);
@@ -27,6 +28,8 @@ int main(void) {
       continue;
     }
     log_client(new_socket, n_clients, client_sockets, &clients_mutex);
+
+    // spawn thread
     client_handler_args_t args = {new_socket, n_clients, client_sockets,
                                   &clients_mutex};
     assign_handler(&args);
