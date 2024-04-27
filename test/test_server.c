fd1099afbd3f0cb9519e5b6115e2ab94d0f5c00d
Ian <ianlum314@gmail.com>
2024-04-27 19:14:29 -0400

    write test for log client

--- test/test_server.c
+++ test/test_server.c
@@ -1,6 +1,7 @@
 #include <criterion/criterion.h>
 #include <criterion/new/assert.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -83,6 +84,55 @@ Test(accept_client, server_accepts) {
     if (waitpid(pid, NULL, 0) == -1) {
       error_and_exit("Error awaiting child process");
     }
+    close_tcp_socket(server->listener);
+    free(server);
+  }
+}
+
+// Check that the server stores the client descriptor
+Test(log_client, client_logged) {
+  // Create server
+  struct sockaddr_in server_addr = socket_address(INADDR_ANY, 6666);
+  server_t* server = init_server(server_addr, MAX_BACKLOG);
+  listen_for_connections(server);
+
+  pid_t pid = -1;
+  pid = fork();
+
+  if (pid < 0) {
+    error_and_exit("Error forking process");
+  } else if (pid == 0) {
+    // Child, client
+    int sock = open_tcp_socket();
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) <
+        0) {
+      close_tcp_socket(sock);
+      error_and_exit("Failed to connect to server");
+    }
+    close_tcp_socket(sock);
+    // NOLINTNEXTLINE(concurrency-mt-unsafe)
+    exit(EXIT_SUCCESS);
+  } else {
+    // Parent, server
+    int new_socket = accept_client(server);
+    // cr_expect(ge(int, accept_client(server), 0));
+    if (new_socket < 0) {
+      error_and_exit("Error accepting client");
+    }
+    if (waitpid(pid, NULL, 0) == -1) {
+      error_and_exit("Error awaiting child process");
+    }
+    // init variables for tracking clients
+    int client_sockets[2];
+    int* n_clients = malloc(sizeof(int));
+    *n_clients = 0;
+    pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    int prior = client_sockets[0];
+    log_client(new_socket, n_clients, client_sockets, &clients_mutex);
+    cr_expect(ne(int, client_sockets[0], prior));
+    cr_expect(eq(int, *n_clients, 1));
+
     close_tcp_socket(server->listener);
     free(server);
   }
