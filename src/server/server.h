3f214a17abfc5eb450838d609f2d6b553a54efa8
Ian <ianlum314@gmail.com>
2024-05-03 13:52:50 -0400

    clang format

--- src/server/server.h
+++ src/server/server.h
@@ -25,22 +25,22 @@ server_t* init_server(struct sockaddr_in ip_addr, int max_backlog);
 
 /**
  * Listens for incoming connections on the server's socket.
- * 
+ *
  * Binds the server's listener socket to the provided address
  * and starts listening for incoming connections. If binding or setting the
  * socket to listen fails, the program exits with an error message.
- * 
+ *
  * @param server Pointer to the server structure.
  */
 void listen_for_connections(server_t* server);
 
 /**
  * Accepts a client connection on the server's listener socket.
- * 
+ *
  * Retrieves information about the connected client and prints it using
  * print_connection(). If accepting the client connection fails, the program
  * exits with an error message.
- * 
+ *
  * @param server Pointer to the server structure.
  * @return The socket descriptor for the connected client.
  */
@@ -49,12 +49,12 @@ int accept_client(server_t* server);
 
 /**
  * Logs a newly connected client.
- * 
+ *
  * This function logs a newly connected client by adding its socket descriptor
  * to the list of client sockets. It locks the clients_mutex to ensure thread
  * safety while accessing and modifying the client_sockets array. Once the
  * client is logged, the mutex is unlocked.
- * 
+ *
  * @param socket_fd The socket descriptor of the newly connected client.
  * @param n_clients Pointer to the number of clients.
  * @param client_sockets Array of client sockets.
